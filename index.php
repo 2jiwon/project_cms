b9bc881294f9ca820e81fb5a40838a566af288d6
2jiwon <ljwjulian@gmail.com>
2018-03-30 19:02:48 +0900

    Missed change index.php to display posts

--- index.php
+++ index.php
@@ -13,7 +13,18 @@ include ('includes/navigation.php');
 
             <!-- Blog Entries Column -->
             <div class="col-md-8">
+<?php
 
+$query = "SELECT * FROM posts";
+$select_all_posts_query = mysqli_query ($connection, $query);
+
+while ($row = mysqli_fetch_assoc ($select_all_posts_query)) {
+  $post_title   = $row['post_title'];
+  $post_author  = $row['post_author'];
+  $post_date    = $row['post_date'];
+  $post_image   = $row['post_image'];
+  $post_content = $row['post_content'];
+?>
                 <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
@@ -21,51 +32,21 @@ include ('includes/navigation.php');
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#">Blog Post Title</a>
+                <a href="#"><?php echo $post_title ?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
+                by <a href="index.php"><?php echo $post_author ?></a>
                 </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
+                <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date ?></p>
                 <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
                 <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+                <p><?php echo $post_content ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
                 <hr>
-
-                <!-- Second Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:45 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, quasi, fugiat, asperiores harum voluptatum tenetur a possimus nesciunt quod accusamus saepe tempora ipsam distinctio minima dolorum perferendis labore impedit voluptates!</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                <hr>
-
-                <!-- Third Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:45 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, voluptates, voluptas dolore ipsam cumque quam veniam accusantium laudantium adipisci architecto itaque dicta aperiam maiores provident id incidunt autem. Magni, ratione.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                <hr>
+<?php
+}
+?>
 
                 <!-- Pager -->
                 <ul class="pager">
@@ -84,7 +65,6 @@ include ('includes/navigation.php');
 include ('includes/sidebar.php'); 
 ?>
             </div>
-
         </div>
         <!-- /.row -->
 
@@ -94,4 +74,3 @@ include ('includes/sidebar.php');
 <?php
 include ('includes/footer.php');
 ?>
-
