684d48008142e95cb91220360144586ed39e45d0
2jiwon <ljwjulian@gmail.com>
2018-04-08 18:03:37 +0900

    Make post links work to lead their individual post.

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ $query = "SELECT * FROM posts";
 $select_all_posts_query = mysqli_query ($connection, $query);
 
 while ($row = mysqli_fetch_assoc ($select_all_posts_query)) {
+  $post_id      = $row['post_id'];
   $post_title   = $row['post_title'];
   $post_author  = $row['post_author'];
   $post_date    = $row['post_date'];
@@ -32,14 +33,14 @@ while ($row = mysqli_fetch_assoc ($select_all_posts_query)) {
 
                 <!-- First Blog Post -->
                 <h2>
-                <a href="#"><?php echo $post_title ?></a>
+                  <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
                 </h2>
                 <p class="lead">
                 by <a href="index.php"><?php echo $post_author ?></a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date ?></p>
                 <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
+                <a href="post.php?p_id=<?php echo $post_id; ?>"><img class="img-responsive" src="images/<?php echo $post_image ?>" alt=""></a>
                 <hr>
                 <p><?php echo $post_content ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
