4e4439e59f40911a957810a1b30d47c679559aaf
2jiwon <ljwjulian@gmail.com>
2018-05-05 12:31:39 +0900

    Added a login panel on the sidebar.

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,5 +1,28 @@
             <div class="col-md-4">
 
+                <!-- Login panel -->
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                      Login
+                    </div> 
+
+                    <!-- Login form -->
+                    <div class="panel-body">
+                      <form action="includes/login.php" method="post">
+                        <div class="form-group"> 
+                          <input class="form-control" name="username" type="text" placeholder="Enter Username">
+                        </div>
+                        <div class="input-group">
+                          <input class="form-control" name="password" type="password" placeholder="Enter Password">
+                          <span class="input-group-btn">
+                            <button class="btn btn-primary" name="login" type="submit">Login</button>
+                          </span>
+                        </div>
+                      <!-- /.input-group -->
+                      </form>
+                    </div>
+                </div>
+
                 <!-- Blog Search Well -->
                 <div class="well">
                     <h4>Blog Search</h4>
