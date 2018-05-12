8d202190c7e879dd781e163b7cbe168d03ffed8f
2jiwon <ljwjulian@gmail.com>
2018-05-12 23:42:10 +0900

    Modified edit_user as password encrypt works.

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -3,7 +3,6 @@
     <tr>
       <th>Id</th>
       <th>Username</th>
-      <th>Password</th>
       <th>First Name</th>
       <th>Last Name</th>
       <th>Email</th>
@@ -31,7 +30,6 @@ if (!$select_all_users) {
   while ($row = mysqli_fetch_assoc ($select_all_users)) {
     $user_id        = $row['user_id'];
     $user_name      = $row['user_name'];
-    $user_password  = $row['user_password'];
     $user_firstname = $row['user_firstname'];
     $user_lastname  = $row['user_lastname'];
     $user_image     = $row['user_image'];
@@ -42,7 +40,6 @@ if (!$select_all_users) {
     echo "<tr>";
     echo "<td>{$user_id}</td>";
     echo "<td>{$user_name}</td>";
-    echo "<td>{$user_password}</td>";
     echo "<td>{$user_firstname}</td>";
     echo "<td>{$user_lastname}</td>";
     echo "<td>{$user_email}</td>";
