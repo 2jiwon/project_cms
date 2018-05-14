6dfe73ce618ab27165cdaa851a1b25fa446f3369
2jiwon <ljwjulian@gmail.com>
2018-05-14 17:20:26 +0900

    Added a loader to admin page.

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -25,3 +25,13 @@ if (selectAll) {
     }
   });
 }
+
+// jQuery for loader
+var div_box = "<div id='load-screen'><div id='loading'></div></div>";
+
+$("body").prepend (div_box);
+
+$('#load-screen').delay (100).fadeOut (500, function (){
+  $(this).remove ();
+});
+
