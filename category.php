bebf70696c60682c682bd18688e13f5bd7c4520c
2jiwon <ljwjulian@gmail.com>
2018-06-25 01:00:59 +0900

    Modified the text style a little.

--- category.php
+++ category.php
@@ -57,23 +57,19 @@ if (isset ($_GET['category'])) {
   if (mysqli_stmt_num_rows ($stmt) > 0) {
     while (mysqli_stmt_fetch ($stmt)) {
 ?>
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
 
                 <!-- First Blog Post -->
                 <h2>
                   <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
                 </h2>
-                <p class="lead">
-                by <a href="author_posts.php?author=<?php echo $post_author; ?>&p_id=<?php echo $post_id; ?>"><?php echo $post_author ?></a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date ?></p>
+                  <p class="lead">
+                  by <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_author ?></a>
+                  </p>
+                  <p class="text-right"><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
                 <hr>
                 <a href="post.php?p_id=<?php echo $post_id; ?>"><img class="img-responsive" src="images/<?php echo $post_image ?>" alt=""></a>
                 <hr>
-                <p><?php echo $post_content . " ..."; ?></p>
+                <p><?php echo substr ($post_content, 0, 100) . " ..."; ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
                 <hr>
 <?php
@@ -96,6 +92,8 @@ if (isset ($_GET['category'])) {
                   <small></small>
             </h1>";
   }
+
+  mysqli_stmt_close ($stmt);
 } // End of fist if
 ?>
             </div>
