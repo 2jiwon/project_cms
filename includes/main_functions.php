55ad9e494979d6fce39aa9d6e7f38712053aaf46
2jiwon <ljwjulian@gmail.com>
2018-08-16 18:55:30 +0900

    Added another helper function.

--- includes/main_functions.php
+++ includes/main_functions.php
@@ -160,4 +160,20 @@ function checkLoggedInAndRedirect ($redirectLocation=null) {
     redirect ($redirectLocation);
   } 
 }
+
+function email_exists($email){
+
+    global $connection;
+
+    $query = "SELECT user_email FROM users WHERE user_email = '$email'";
+    $result = mysqli_query($connection, $query);
+    confirmQuery($result);
+
+    if(mysqli_num_rows($result) > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 ?>
