0d5f897ec4bd8ce97d7f1a1b5e9b19a4379b8311
2jiwon <ljwjulian@gmail.com>
2018-08-16 23:23:44 +0900

    Added a query to update token value in database.

--- forgot.php
+++ forgot.php
@@ -1,9 +1,41 @@
-<?php  include "includes/db.php"; ?>
-<?php  include "includes/header.php"; ?>
+<?php
+include "includes/db.php";
+include "includes/header.php";
+include "includes/main_functions.php";
+?>
 
 <!-- Navigation -->
 <?php  include "includes/navigation.php"; ?>
 
+<?php 
+  if (!IsItMethod('get') && !isset($_GET['id'])) {
+    redirect ('index');
+  }
+?>
+
+<?php
+
+if (IsItMethod('post')) {
+  if (isset($_POST['email'])) {
+
+    $email = $_POST['email'];
+    $length = 50;
+    $token = bin2hex(openssl_random_pseudo_bytes($length));  
+
+    if (field_exists ($email, 'user_email')) {
+      $query = "UPDATE users SET token='{$token}' WHERE user_email = ? ";
+
+      if ($stmt  = $connection->prepare ($query)) {
+        $stmt->bind_param ("s", $email);
+        $stmt->execute();
+      } else {
+        echo "Something's gone wrong.";
+      }
+    }
+  }
+}
+
+?>
 <!-- Page Content -->
 <div class="container">
 
@@ -15,11 +47,11 @@
           <div class="panel-body">
             <div class="text-center">
 
-
               <h3><i class="fa fa-lock fa-4x"></i></h3>
               <h2 class="text-center">Forgot Password?</h2>
               <p>You can reset your password here.</p>
               <div class="panel-body">
+
 
                 <form id="register-form" role="form" autocomplete="off" class="form" method="post">
 
