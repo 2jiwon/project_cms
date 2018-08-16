9e791d69dfe0eb2a67f5a8aee1838d04ee6f5eba
2jiwon <ljwjulian@gmail.com>
2018-08-16 16:36:15 +0900

    Modified the indentation.

--- login.php
+++ login.php
@@ -12,17 +12,17 @@ include "includes/main_functions.php";
 <div class="container">
 
 	<div class="form-gap"></div>
-	<div class="container">
+	  <div class="container">
 		<div class="row">
-			<div class="col-md-4 col-md-offset-4">
-				<div class="panel panel-default">
-					<div class="panel-body">
-						<div class="text-center">
+		  <div class="col-md-4 col-md-offset-4">
+		    <div class="panel panel-default">
+			  <div class="panel-body">
+			    <div class="text-center">
 
 
-							<h3><i class="fa fa-user fa-4x"></i></h3>
-							<h2 class="text-center">Login</h2>
-							<div class="panel-body">
+			    <!-- <h3><i class="fa fa-user fa-4x"></i></h3> -->
+				<h2 class="text-center">Login</h2>
+				<div class="panel-body">
 
 <?php
   
@@ -37,42 +37,40 @@ if (IsItMethod ('post')) {
 }
 ?>
 
+				  <form id="login-form" role="form" autocomplete="off" class="form" method="post">
 
-								<form id="login-form" role="form" autocomplete="off" class="form" method="post">
+				    <div class="form-group">
+					  <div class="input-group">
+						<span class="input-group-addon"><i class="glyphicon glyphicon-user color-blue"></i></span>
 
-									<div class="form-group">
-										<div class="input-group">
-											<span class="input-group-addon"><i class="glyphicon glyphicon-user color-blue"></i></span>
+						<input name="username" type="text" class="form-control" placeholder="Enter Username">
+					  </div>
+					</div>
 
-											<input name="username" type="text" class="form-control" placeholder="Enter Username">
-										</div>
-									</div>
-
-									<div class="form-group">
-										<div class="input-group">
-											<span class="input-group-addon"><i class="glyphicon glyphicon-lock color-blue"></i></span>
-											<input name="password" type="password" class="form-control" placeholder="Enter Password">
-										</div>
-									</div>
-
-									<div class="form-group">
-
-										<input name="login" class="btn btn-lg btn-primary btn-block" value="Login" type="submit">
-									</div>
-
-
-								</form>
-
-							</div><!-- Body-->
-
+                    <div class="form-group">
+    					<div class="input-group">
+	                      <span class="input-group-addon"><i class="glyphicon glyphicon-lock color-blue"></i></span>
+						  <input name="password" type="password" class="form-control" placeholder="Enter Password">
 						</div>
 					</div>
-				</div>
+
+					<div class="form-group">
+                      <input name="login" class="btn btn-lg btn-primary btn-block" value="Login" type="submit">
+					</div>
+
+
+				  </form>
+
+				</div><!-- Body-->
+
+			  </div>
 			</div>
 		</div>
+	  </div>
 	</div>
+  </div>
 
-	<hr>
+  <hr>
 
 	<?php include "includes/footer.php";?>
 
