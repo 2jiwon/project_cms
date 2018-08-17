2643ed4aa0bee09ced50a5540250b9926cb3112b
2jiwon <ljwjulian@gmail.com>
2018-08-17 19:31:24 +0900

    Modified the styles of the sending mail result message.

--- forgot.php
+++ forgot.php
@@ -22,19 +22,6 @@ include "includes/main_functions.php";
   }
 ?>
 
-<!-- Page Content -->
-<div class="container">
-
-  <div class="form-gap"></div>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-4 col-md-offset-4">
-        <div class="panel panel-default">
-          <div class="panel-body">
-            <div class="text-center">
-              <h3><i class="fa fa-lock fa-4x"></i></h3>
-              <h2 class="text-center">Forgot Password?</h2>
-              <p>You can reset your password here.</p>
 <?php
 
 if (IsItMethod('post')) {
@@ -79,8 +66,9 @@ if (IsItMethod('post')) {
             $mail->Body = "<p><a href='http://uphp.com/cms/reset.php?email={$email}&token={$token}'>
               Please click reset your password.</a></p>";
   
-            $mail->send();
-            echo "<div class='btn btn-info'>Message has been sent</div>";
+            if ($mail->send()) {
+              $emailSent = true;
+            }
         } catch (Exception $e) {
             echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
         }
@@ -91,6 +79,24 @@ if (IsItMethod('post')) {
   }
 }
 ?>
+
+<!-- Page Content -->
+<div class="container">
+
+  <div class="form-gap"></div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 col-md-offset-4">
+        <div class="panel panel-default">
+      
+          <div class="panel-body">
+            <div class="text-center">
+              <h3><i class="fa fa-lock fa-4x"></i></h3>
+              <h2 class="text-center">Forgot Password?</h2>
+              <p>You can reset your password here.</p>
+
+<?php if (!isset($emailSent)): ?>
+
               <div class="panel-body">
                 <form id="register-form" role="form" autocomplete="off" class="form" method="post">
 
@@ -107,10 +113,15 @@ if (IsItMethod('post')) {
                   <input type="hidden" class="hide" name="token" id="token" value="">
                 </form>
 
-              </div><!-- /.panel-body-->
+              </div><!-- /.panel-body -->
+<?php else: ?>
+
+          <div class='alert alert-success'> Mail has been sent. Please check your email.</div>
+
+<?php endIf; ?>
 
             </div>
-          </div>
+          </div><!-- /.panel-body -->
         </div>
       </div>
     </div>
