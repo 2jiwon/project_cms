4c092d2c3d9cd9b545fa4b4334b74649e012503b
2jiwon <ljwjulian@gmail.com>
2018-08-17 19:57:22 +0900

    Added a reset password page.

---
+++ reset.php
@@ -0,0 +1,63 @@
+<?php
+include "includes/db.php";
+include "includes/header.php";
+include "includes/main_functions.php";
+?>
+
+<!-- Navigation -->
+<?php  include "includes/navigation.php"; ?>
+
+<!-- Page Content -->
+<div class="container">
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 col-md-offset-4">
+        <div class="panel panel-default">
+          <div class="panel-body">
+            <div class="text-center">
+
+              <h3><i class="fa fa-lock fa-4x"></i></h3>
+              <h2 class="text-center">Reset Password</h2>
+              <p>You can reset your password here.</p>
+              <div class="panel-body">
+
+
+                <form id="register-form" role="form" autocomplete="off" class="form" method="post">
+
+                  <div class="form-group">
+                    <div class="input-group">
+                      <span class="input-group-addon"><i class="glyphicon glyphicon-user color-blue"></i></span>
+                      <input id="password" name="password" placeholder="Enter password" class="form-control"  type="password">
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="input-group">
+                      <span class="input-group-addon"><i class="glyphicon glyphicon-ok color-blue"></i></span>
+                      <input id="confirmPassword" name="confirmPassword" placeholder="Confirm password" class="form-control"  type="password">
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <input name="resetPassword" class="btn btn-lg btn-primary btn-block" value="Reset Password" type="submit">
+                  </div>
+
+                    <input type="hidden" class="hide" name="token" id="token" value="">
+                </form>
+
+              </div><!-- Body-->
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<hr>
+
+<?php include "includes/footer.php";?>
+
+</div> <!-- /.container -->
+
