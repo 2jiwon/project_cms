37848663296c09e1c0f838370fd0ab5302de16c6
2jiwon <ljwjulian@gmail.com>
2018-08-17 23:47:47 +0900

    Added 'forgot password' link to login page as well.

--- login.php
+++ login.php
@@ -7,23 +7,6 @@ include "includes/main_functions.php";
 <!-- Navigation -->
 <?php  include "includes/navigation.php"; ?>
 
-
-<!-- Page Content -->
-<div class="container">
-
-	<div class="form-gap"></div>
-	  <div class="container">
-		<div class="row">
-		  <div class="col-md-4 col-md-offset-4">
-		    <div class="panel panel-default">
-			  <div class="panel-body">
-			    <div class="text-center">
-
-
-			    <!-- <h3><i class="fa fa-user fa-4x"></i></h3> -->
-				<h2 class="text-center">Login</h2>
-				<div class="panel-body">
-
 <?php
   
 checkLoggedInAndRedirect ('${home_url}/admin');
@@ -37,34 +20,50 @@ if (IsItMethod ('post')) {
 }
 ?>
 
-				  <form id="login-form" role="form" autocomplete="off" class="form" method="post">
+<!-- Page Content -->
+<div class="container">
 
-				    <div class="form-group">
-					  <div class="input-group">
-						<span class="input-group-addon"><i class="glyphicon glyphicon-user color-blue"></i></span>
+	<div class="form-gap"></div>
+	  <div class="container">
+		<div class="row">
+		  <div class="col-md-4 col-md-offset-4">
+		    <div class="panel panel-default">
+			  <div class="panel-body">
+			    <div class="text-center">
 
-						<input name="username" type="text" class="form-control" placeholder="Enter Username">
-					  </div>
-					</div>
+                  <!-- <h3><i class="fa fa-user fa-4x"></i></h3> -->
+                  <h2 class="text-center">Login</h2>
+                  <div class="panel-body">
 
-                    <div class="form-group">
-    					<div class="input-group">
-	                      <span class="input-group-addon"><i class="glyphicon glyphicon-lock color-blue"></i></span>
-						  <input name="password" type="password" class="form-control" placeholder="Enter Password">
-						</div>
-					</div>
+                    <form id="login-form" role="form" autocomplete="off" class="form" method="post">
+                      <div class="form-group">
+                        <div class="input-group">
+                          <span class="input-group-addon"><i class="glyphicon glyphicon-user color-blue"></i></span>
+                          <input name="username" type="text" class="form-control" placeholder="Enter Username">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <div class="input-group">
+                          <span class="input-group-addon"><i class="glyphicon glyphicon-lock color-blue"></i></span>
+                          <input name="password" type="password" class="form-control" placeholder="Enter Password">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <input name="login" class="btn btn-lg btn-primary btn-block" value="Login" type="submit">
+                      </div>
+                    </form>
 
-					<div class="form-group">
-                      <input name="login" class="btn btn-lg btn-primary btn-block" value="Login" type="submit">
-					</div>
+                  </div><!-- ./panel-body-->
+			    </div>
+              </div><!-- ./panel-body-->
 
+              <div class="panel-footer">
+                <div class="form-group text-right">
+                  <a href="forgot.php?id=<?php echo uniqid(true); ?>">Forgot Password?</a>
+                </div>
+              </div>
 
-				  </form>
-
-				</div><!-- Body-->
-
-			  </div>
-			</div>
+		  </div>
 		</div>
 	  </div>
 	</div>
