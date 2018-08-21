e739183cf816c609fecf8bfcfb74cd14e1271d76
2jiwon <ljwjulian@gmail.com>
2018-08-21 17:36:23 +0900

    Created a new table 'likes'.

--- assets/02_create_tb_posts.sql
+++ assets/02_create_tb_posts.sql
@@ -1,6 +1,6 @@
 CREATE TABLE posts (
-  post_id INT(3) NOT NULL AUTO_INCREMENT PRIMARY KEY
-  ,post_category_id INT(3) NOT NULL
+  post_id TINYINT(3) NOT NULL AUTO_INCREMENT PRIMARY KEY
+  ,post_category_id TINYINT(3) NOT NULL
   ,post_title VARCHAR(255) NOT NULL
   ,post_author VARCHAR(255) NOT NULL
   ,post_date DATETIME NOT NULL
