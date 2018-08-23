788e4ed9d08205206562b4f651bcc36d6d42554e
2jiwon <ljwjulian@gmail.com>
2018-08-23 18:17:49 +0900

    Fixed the wrong paths in sidebar.php.

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -4,15 +4,15 @@
 <?php if (isset ($_SESSION['user_role'])) : ?>
                 <div class="well">
                    <h4>Logged in as <?php echo $_SESSION['username']; ?></h4>
-                   <a class="btn btn-warning" href="includes/logout.php">Log Out</a> 
+                   <a class="btn btn-warning" href="<?php echo $home_url; ?>/includes/logout.php">Log Out</a> 
                 </div>
 <?php else: ?>
                 <div class="collapse panel panel-primary" id="collapseLogin">
-                    <div class="panel-heading"><a class="btn-primary" href="login.php"> Log In </a></div> 
+                <div class="panel-heading"><a class="btn-primary" href="<?php echo $home_url; ?>/login"> Log In </a></div> 
 
                     <!-- Login form -->
                     <div class="panel-body">
-                      <form action="includes/login.php" method="post">
+                    <form action="<?php echo $home_url; ?>/login" method="post">
                         <div class="form-group"> 
                           <input class="form-control" name="username" type="text" placeholder="Enter Username">
                         </div>
@@ -69,7 +69,7 @@ mysqli_stmt_store_result ($stmt);
 mysqli_stmt_bind_result ($stmt, $cat_id, $cat_title);
 
 while (mysqli_stmt_fetch ($stmt)) {
-    echo "<li><a href='category.php?category={$cat_id}'>{$cat_title}</a></li>";
+    echo "<li><a href='{$home_url}/category.php?category={$cat_id}'>{$cat_title}</a></li>";
 }
 
 ?>
